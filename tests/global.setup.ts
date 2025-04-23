89acbc81cc40a5ae35ad1b0ac6131170d550c9be
Kriksy <viktoria.toldi@gmail.com>
2025-04-23 21:51:31 +0200

    Add global setup test

---
+++ tests/global.setup.ts
@@ -0,0 +1,32 @@
+import {
+  test as setup,
+  expect,
+  request,
+  APIRequestContext,
+} from "@playwright/test";
+import { faker } from "@faker-js/faker";
+import path from "path";
+import fs from "fs";
+
+const authFile = path.join(__dirname, "../playwright/.auth/user.json");
+
+// Run before all tests in file
+setup("Login", async ({ playwright, request }) => {
+  const loginResponse = await request.post("/api/login", {
+    data: {
+      username: process.env.TEST_USERNAME,
+      password: process.env.TEST_PASSWORD,
+    },
+  });
+
+  expect(loginResponse.ok()).toBeTruthy();
+  expect(loginResponse.status()).toBe(200);
+
+  const { token, username } = await loginResponse.json();
+
+  fs.writeFile(authFile, JSON.stringify({ username, token }), (err) => {
+    if (err) {
+      throw new Error(`Failed to write to file: ${err}`);
+    }
+  });
+});
